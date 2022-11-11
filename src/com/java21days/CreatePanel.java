eebc65e87e073150739060200fdfd33d9a34c92a
Alejandro Salas <asalas.wd@outlook.com>
2022-11-10 21:22:10 -0500

    Add panel elements to main class

--- src/com/java21days/CreatePanel.java
+++ src/com/java21days/CreatePanel.java
@@ -1,4 +1,24 @@
 package com.java21days;
 
+import javax.swing.*;
+
 public class CreatePanel {
+    public static void main(String[] args) {
+        JPanel panel = new JPanel();
+        JScrollPane scrollerPanel = new JScrollPane(
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED
+        );
+
+        JLabel titleLabel = new JLabel("Title");
+        JTextField title = new JTextField(15);
+
+        JLabel commentsLabel = new JLabel("Comments");
+        JTextArea comments = new JTextArea(15, 15);
+
+
+
+        ScrollingPanel scroller = new ScrollingPanel("Scroller");
+
+    }
 }
--- src/com/java21days/ScrollingPanel.java
+++ src/com/java21days/ScrollingPanel.java
@@ -1,9 +1,16 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ScrollingPanel extends JFrame {
+    Dimension dim = new Dimension(500, 500);
+
     ScrollingPanel(String title){
         super(title);
+        setSize(dim);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
 }
